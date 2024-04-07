8d85657204610bfdbf807e4615238b8bf7893852
Mary McDonnell <G00401878@atu.ie>
2024-04-07 21:52:49 +0100

    Changed ui for food pantry

--- src/FrontendComponents/FoodPantry.js
+++ src/FrontendComponents/FoodPantry.js
@@ -1,41 +1,22 @@
 import React from 'react';
 import '../CssFiles/Pantry.css';
-import { Container, Dropdown } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 //This page is static information about the Food Pantry
 const FoodPantry = () => {
     return (
         <div className="PantryPage">
-
-            {/* https://react-bootstrap.netlify.app/docs/components/dropdowns */}
-            {/* Make 3 buttons that when clicked display the answer to the question on the button with a dropdown menu*/}
+            {/* Changed to headers for easier reading as requested */}
             <Container className="dropdownsContainer" fluid>
-                <Dropdown>
-                    <Dropdown.Toggle className="pantryButtons" id="dropdown-basic">
-                        What is it?
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu fluid>
-                        <Dropdown.Item href="#">ATU and FoodCloud<br></br> have made a service <br></br>fill your tote bag with<br></br> quality food items.</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                
+                <h1>What is it?</h1>
+                <h3>ATU and FoodCloud have made a service fill your tote bag with quality food items.</h3>
 
-                <Dropdown>
-                    <Dropdown.Toggle className="pantryButtons" variant="primary" id="dropdown-where">
-                        Where is it?
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#">Unit 3 Room 623A from <br></br>1pm - 3pm on Wednesdays!</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                <h1>Where and When is it??</h1>
+                <h3>Unit 3 Room 623A from 1pm - 3pm on Wednesdays!</h3>
 
-                <Dropdown>
-                    <Dropdown.Toggle className="pantryButtons" variant="primary" id="dropdown-who">
-                        Who is it for?
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#">Any student with €5 cash<br></br> and a tote bag!</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                <h1>Who is it for?</h1>
+                <h3>Any student with €5 cash and a tote bag!</h3>
             </Container>
 
 
